65a6f8cd130a48d918612b5c3034e0455aacc60a
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-10-17 20:37:33 +0200

    Update Codes_UMAP_t-SNE.py

--- Codes_UMAP_t-SNE.py
+++ Codes_UMAP_t-SNE.py
@@ -1,1 +1,55 @@
+#Code pour représenter le graphe de l'algorithme UMAP sur les données "digits"
+import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import umap.umap_ as umap
+%matplotlib inline
+sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
+digits = load_digits()
+fig, ax_array = plt.subplots(20, 20)
+axes = ax_array.flatten()
+for i, ax in enumerate(axes):
+    ax.imshow(digits.images[i], cmap='gray_r')
+plt.setp(axes, xticks=[], yticks=[], frame_on=False)
+plt.tight_layout(h_pad=0.5, w_pad=0.01)
 
+reducer = umap.UMAP(random_state=42)
+reducer.fit(digits.data)
+
+embedding = reducer.transform(digits.data)
+# Verify that the result of calling transform is
+# idenitical to accessing the embedding_ attribute
+assert(np.all(embedding == reducer.embedding_))
+
+plt.scatter(embedding[:, 0], embedding[:, 1], c=digits.target, cmap='Spectral', s=5)
+plt.gca().set_aspect('equal', 'datalim')
+plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(10))
+plt.title('UMAP projection of the Digits dataset', fontsize=24);
+
+
+
+
+
+
+#Code pour représenter le graphe de l'algorithme t-SNE sur les données "digits"
+import numpy as np
+from sklearn.datasets import load_digits
+from scipy.spatial.distance import pdist
+from sklearn.manifold.t_sne import _joint_probabilities
+from scipy import linalg
+from sklearn.metrics import pairwise_distances
+from scipy.spatial.distance import squareform
+from sklearn.manifold import TSNE
+from matplotlib import pyplot as plt
+import seaborn as sns
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+palette = sns.color_palette("bright", 10)
+
+tsne = TSNE()
+X_embedded = tsne.fit_transform(X)
+
+sns.scatterplot(X_embedded[:,0], X_embedded[:,1], hue=y, legend='full', palette=palette)
